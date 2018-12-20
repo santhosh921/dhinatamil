84913a50b5a39fc85d428d9e461bee7bb8ccfd9d
pavithra1909 <43338282+pavithra1909@users.noreply.github.com>
2018-12-20 14:29:11 +0530

    Create p1.c

---
+++ p1.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a,b,sum;
+printf("\n enter the  a value");
+scanf("%d",&a);
+printf("\n enter the b value");
+scanf("%d",&b);
+printf("\n sum is:%d",sum);
+getch();
+}
